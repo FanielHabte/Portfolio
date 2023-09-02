b58960d961cf5e6b73ab0b59e29e4e9691019ba5
FanielHabte <fanielhabte20@gmail.com>
2023-09-02 02:11:39 -0700

    header and section 1 and 2 updates

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; // Use port 3000 as a fallback
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// const port = process.env.PORT || 3000; // Use port 3000 as a fallback
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
 
 app.set("view engine", "ejs");
 
@@ -24,32 +24,18 @@ app.get('/About', (req, res) => {
 });
 
 //
-app.get('/Contact', (req, res) => {
+app.get('/Portofolio', (req, res) => {
 
-  let title = "Contact"
-  res.render("pages/Contact", {"title": title} );
+  let title = "Portofolio"
+  res.render("pages/Portofolio", {"title": title} );
 });
 
 //
-app.get('/Locations', (req, res) => {
 
-  let title = "Locations "
-  res.render("pages/Locations", {"title": title} );
+
+
+const port = 3000; // Change this to a port that is not in use
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
-//socialism page 
-app.get('/News', (req, res) => {
-
-  let title = "News"
-  res.render("pages/News", {"title": title} );
-});
-
-//contact page
-app.get('/Services', (req, res) => {
-
-  let title = "Services"
-  res.render("pages/Services", {"title": title} );
-});
-
-
-app.listen(5000)
