4ba552977f5d89b0a37a68147f486285bd0ccd52
FanielHabte <fanielhabte20@gmail.com>
2023-09-02 03:04:14 -0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-// const port = process.env.PORT || 3000; // Use port 3000 as a fallback
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+const port = process.env.PORT || 3000; // Use port 3000 as a fallback
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 app.set("view engine", "ejs");
 
@@ -34,8 +34,8 @@ app.get('/Portofolio', (req, res) => {
 
 
 
-const port = 3000; // Change this to a port that is not in use
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// const port = 3000; // Change this to a port that is not in use
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
 
